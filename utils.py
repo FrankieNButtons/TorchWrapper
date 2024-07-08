eac48faa3b4c98cd45e6dbd3b4711a99d1828a27
Frankie Buttons <62317502+FrankieNButtons@users.noreply.github.com>
2024-07-08 10:52:55 +0800

    Update utils.py
    update getAPIName, getAttributes, and isDecorated.

--- utils.py
+++ utils.py
@@ -1,29 +1,37 @@
 """
 All the packed universal functions were stored here.
 """
+import types;
+
+
+
+
+
 def isFuncFromModule(func: types.FunctionType, module: str):
     return func.__module__.startswith(module);
-isFuncFromModule(torch.nn.LSTM, "torch")
+# isFuncFromModule(torch.nn.LSTM, "torch")
 
 
-def getAttributes(module: str):
-    return [i for i in dir(module) if "__" not in i];
-getAttributes(torch.optim)
+def getAttributes(module):
+    return [i for i in dir(module) if not i.startswith("__") and callable(getattr(module, i)) or isinstance(getattr(module, i), types.ModuleType)];
+# getAttributes(torch.optim)
 
 
 def getAPIName(func):
-    apiName = func.__module__;
-    if func.__name__[-len(func.__name__)+1:] == apiName[-len(func.__name__)+1:]:
-        apiName = apiName[:-len(func.__name__)] + func.__name__;
-    else:
-        apiName = apiName + "." + func.__name__;
-    return apiName;
-getAPIName(torch.optim.Adam),getAPIName(torch.mul)
+    if isinstance(func, types.FunctionType) or isinstance(func, types.BuiltinFunctionType):
+        apiName = func.__module__ + "." + func.__name__;
+        return apiName;
+    elif isinstance(func, type):
+        apiName = func.__module__;
+        if func.__name__[-len(func.__name__)+1:] == apiName[-len(func.__name__)+1:]:
+            apiName = apiName[:-len(func.__name__)] + func.__name__;
+        return apiName;
+    elif isinstance(func, types.ModuleType):
+        return func.__name__;
+# getAPIName(torch.optim.Adam),getAPIName(torch.mul)
 
 
-def isDecorated(func: types.FunctionType):
-    if hasattr(func, "isDecorated"):
-        return True;
-    else: 
-        return False;
-isDecorated(torch.tensor)
+
+def isDecorated(func):
+    return getattr(func, "isDecorated", False);
+ # isDecorated(torch.tensor)
